bd8657223700be9f96e79ebf0ba9bd2990a3aad0
Joss Rider <jossrider@outlook.com>
2024-05-15 22:25:50 -0400

    Agregamos boton para reinicar app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useReducer } from "react";
+import { useEffect, useMemo, useReducer } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
+
+  const canResetApp = () =>
+    useMemo(() => state.activities.length, [state.activities]);
 
   return (
     <>
@@ -13,15 +20,22 @@ function App() {
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorias
           </h1>
+          <button
+            className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
+            disabled={!canResetApp()}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form dispatch={dispatch} />
+          <Form dispatch={dispatch} state={state} />
         </div>
       </section>
       <section className="p-10 mx-auto max-w-4l">
-        <ActivityList activities={state.activities} />
+        <ActivityList activities={state.activities} dispatch={dispatch} />
       </section>
     </>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,13 @@
-import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react";
 import { v4 as uuuiv4 } from "uuid";
 import { categories } from "../data/categories";
 import type { Activity } from "../types";
-import { ActivityActions } from "../reducers/activity-reducer";
+import { ActivityState } from "../reducers/activity-reducer";
+import { ActivityActions } from "../reducers/ActivityActions";
 
 type FormProps = {
   dispatch: Dispatch<ActivityActions>;
+  state: ActivityState;
 };
 
 const initialState: Activity = {
@@ -15,8 +17,17 @@ const initialState: Activity = {
   calories: 0,
 };
 
-export default function Form({ dispatch }: FormProps) {
+export default function Form({ dispatch, state }: FormProps) {
   const [activity, setActivity] = useState<Activity>(initialState);
+
+  useEffect(() => {
+    if (state.activeId) {
+      const selectedActivity = state.activities.filter(
+        (stateActivity) => stateActivity.id === state.activeId
+      )[0];
+      setActivity(selectedActivity);
+    }
+  }, [state.activeId]);
 
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
