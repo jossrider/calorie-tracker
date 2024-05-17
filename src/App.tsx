051241fad89bf1f90e7eb07736f2ba5ef6dc1a3d
Joss Rider <jossrider@outlook.com>
2024-05-16 21:00:45 -0400

    Actualizacion rutinaria

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useMemo, useReducer } from "react";
 import Form from "./components/Form";
-import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
+import { activityReducer, initialState } from "./reducers/activity-reducer";
+import CalorieTracker from "./components/CalorieTracker";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -32,6 +33,11 @@ function App() {
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
+        </div>
+      </section>
+      <section className="bg-gray-800 p-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker activities={state.activities} />
         </div>
       </section>
       <section className="p-10 mx-auto max-w-4l">
